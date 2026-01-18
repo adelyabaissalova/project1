67e87d2be9ecf4deac76da923794f773af7e422a
inkar <inkarotyzbaeva0107@gmail.com>
2026-01-18 16:29:43 +0500

    changed book class

--- src/models/Book.java
+++ src/models/Book.java
@@ -1,4 +1,33 @@
-package models;
+package model;
 
 public class Book {
+    private int id;
+    private String title;
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Book(int id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
