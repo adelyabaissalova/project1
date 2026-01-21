b47181c613eb53a724523c80210af9a59d38a970
yeldana <plesetckijuri@gmail.com>
2026-01-21 15:05:34 +0600

    commit2

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -14,7 +14,7 @@ public class MyApplication {
         while (true) {
             System.out.println("\n=== Online library ===");
             System.out.println("1. Add book");
-            System.out.println("2. Show all books");
+            System.out.println("2. Demonstrate all books");
             System.out.println("3. Mark as read");
             System.out.println("4. Mark as not read");
             System.out.println("0. Exit");
