3a2cbbfb97ac10d43a4fc3b803eef82bcd7e6b01
adelyabaissalova <adelyabaysalova@gmail.com>
2026-01-31 22:20:42 +0500

    implemented project 1

--- src/models/Author.java
+++ src/models/Author.java
@@ -5,13 +5,8 @@ public class Author {
     private String fullName;
 
     public Author() {}
-
-    public Author(int id, String fullName) {
-        this.id = id;
-        this.fullName = fullName;
-    }
-
-    public int getId() { return id; }
+}
+ public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
     public String getFullName() { return fullName; }
