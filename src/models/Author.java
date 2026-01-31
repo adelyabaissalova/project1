ca1cde257234a904ad3a6a0fbc731b56e9337d5a
adelyabaissalova <adelyabaysalova@gmail.com>
2026-01-31 22:16:56 +0500

    implemented project 1

---
+++ src/models/Author.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Author {
+    private int id;
+    private String fullName;
+
+    public Author() {}
+
+    public Author(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    @Override
+    public String toString() {
+        return id + " | " + fullName;
+    }
+}
