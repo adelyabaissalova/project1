2ee167af1d7316a9c12ceeeac6b41e9f4bacbcf6
inkar <inkarotyzbaeva0107@gmail.com>
2026-02-06 21:54:37 +0500

    authorization

--- src/security/AuthService.java
+++ src/security/AuthService.java
@@ -7,8 +7,8 @@ public class AuthService {
     private final Map<String, User> users = new HashMap<>();
 
     public AuthService() {
-        users.put("Friend1", new User("Yeldana", "admin123", Role.ADMIN));
-        users.put("Friend2", new User("Adelya", "admin123", Role.ADMIN));
+        users.put("Yeldana", new User("Yeldana", "admin123", Role.ADMIN));
+        users.put("Adelya", new User("Adelya", "admin123", Role.ADMIN));
         users.put("Inkar", new User("Inkar", "admin123", Role.ADMIN));
         users.put("librarian",  new User("librarian",  "lib123",        Role.LIBRARIAN));
         users.put("user",       new User("user",       "user123",       Role.USER));
